3fc472cd3752b72e5ceda27e6314b5ecf258ea00
TakWolf <takwolf@foxmail.com>
2016-05-14 06:01:28 +0800

    header基本布局完成

--- js/topic.js
+++ js/topic.js
@@ -13,6 +13,49 @@ $(document).ready(function () {
     });
 });
 
+var MINUTE = 60 * 1000;
+var HOUR = 60 * MINUTE;
+var DAY = 24 * HOUR;
+var WEEK = 7 * DAY;
+var MONTH = 31 * DAY;
+var YEAR = 12 * MONTH;
+
+Vue.filter('recentlyTimeText', function (time) {
+    var offset = Date.parse(new Date()) - Date.parse(new Date(time));
+    if (offset > YEAR) {
+        return Math.floor(offset / YEAR) + "年前";
+    } else if (offset > MONTH) {
+        return Math.floor(offset / MONTH) + "个月前";
+    } else if (offset > WEEK) {
+        return Math.floor(offset / WEEK) + "周前";
+    } else if (offset > DAY) {
+        return Math.floor(offset / DAY) + "天前";
+    } else if (offset > HOUR) {
+        return Math.floor(offset / HOUR) + "小时前";
+    } else if (offset > MINUTE) {
+        return Math.floor(offset / MINUTE) + "分钟前";
+    } else {
+        return "刚刚";
+    }
+});
+
+Vue.filter('tabName', function (topic) {
+    if (topic.top) {
+        return '置顶';
+    } else {
+        switch (tab) {
+            case 'share':
+                return '分享';
+            case 'ask':
+                return '问答';
+            case 'job':
+                return '招聘';
+            default:
+                return '全部';
+        }
+    }
+});
+
 var vmPage = new Vue({
     el: '#page',
     data: {
@@ -22,7 +65,7 @@ var vmPage = new Vue({
     },
     methods: {
 
-        collectTopic: function () {
+        collectTopic: function (topic) {
             if (topic.is_collect) {
                 window.topicBridge.decollectTopic(topic.id);
             } else {
